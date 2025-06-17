72ec14b2721afed057ba69e459ec693b65860a1e
ysl2 <songliyu97@gmail.com>
2025-06-17 13:11:37 +0800

    Work in progress.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import webdriver_manager.chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import requests
+import json
+from tqdm.auto import tqdm
 
 
 def main():
@@ -12,15 +15,28 @@ def main():
     driver = selenium.webdriver.Chrome(service=service, options=options)
 
     driver.get('https://www.markji.com/deck/677629bf4b4cda45377c7a6e')
-    res = WebDriverWait(driver, 10).until(
+    chapter_with_cards = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@class="chapter-with-cards"]'))
     )
     with open('README1.md', 'w') as f:
-        for row in res.find_elements(By.XPATH, './li'):
+        for row in tqdm(chapter_with_cards.find_elements(By.XPATH, './li')):
             row_class = row.get_attribute('class')
             row_text = row.find_element(By.XPATH, './div/span[2]').text
             if row_class == 'chapter-item':
                 row_text = row_text.split('(')[0]
+            else:
+                row_idstr = row_text.split('.')[0]
+                row_engurl = requests.get(f'http://127.0.0.1:8000/problem/{row_idstr}').text
+                row_engurl = json.loads(row_engurl)['url']
+                row_engtitle = row_engurl.split('/')[-2]
+                row_cnurl = f'https://leetcode.cn/problems/{row_engtitle}/description'
+                driver.get(row_cnurl)
+                row_cntitle = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, f'//*[@href="/problems/{row_engtitle}/"]'))
+                ).text
+                if row_text == row_cntitle:
+                    row_text = f'[{row_text}({row_cnurl})]'
+            print(row_text)
             f.write(f'{"##" if row_class == "chapter-item" else "###"} {row_text}\n')
 
 
